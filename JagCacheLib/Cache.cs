8f0721d876681f5fbb6ca1d038064dc593cf858c
Ronnie Tran <ronnie.tran2@gmail.com>
2020-03-24 16:27:55 -0400

    Add basic file reading

--- JagCacheLib/Cache.cs
+++ JagCacheLib/Cache.cs
@@ -1,44 +1,150 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
 namespace JagCacheLib
 {
-    public class Cache
+    public class Cache : IDisposable
     {
-        private const string DataFileName = "main_file_cache.dat2";
+        private const string DataFileName = "main_file_cache.dat";
         private const string IndexFilePrefixName = "main_file_cache.idx";
-        private const byte MasterIndexId = 255;
-        
+        private const byte MaxIndexCount = 255;
+
+        private const byte BlockHeaderSize = 8;
+        private const byte BlockHeaderExtendedSize = 10;
+
+        private const ushort BlockChunkSize = 512;
+        private const ushort BlockChunkExtendedSize = 510;
+
+        private const ushort TotalBlockSize = 520;
+
         private readonly FileStream _mainDataFileStream;
-        private readonly FileStream _masterIndexFileStream;
-        public Index[] Indices { get; }
-        public Index MasterIndex { get; }
-        
+        private readonly Dictionary<int, Index> _indices;
+
+        private readonly struct Header
+        {
+            public Header(byte[] data, bool large)
+            {
+                if (data.Length == 0)
+                {
+                    throw new Exception("Cannot decode header. Given data cannot be empty.");
+                }
+
+                if (data.Length != BlockHeaderSize && data.Length != BlockHeaderExtendedSize)
+                {
+                    throw new Exception($"Invalid header size of {data.Length} given.");
+                }
+
+                if (large)
+                {
+                    NextEntryId = data[0] << 24 | data[1] << 16 | data[2] << 8 |
+                                  data[3];
+                    NextSequence = data[4] << 8 | data[5];
+                    NextBlock = data[6] << 16 | data[7] << 8 | data[8];
+                    NextIndexId = data[9];
+                }
+                else
+                {
+                    NextEntryId = data[0] << 8 | data[1];
+                    NextSequence = data[2] << 8 | data[3];
+                    NextBlock = data[4] << 16 | data[5] << 8 | data[6];
+                    NextIndexId = data[7];
+                }
+            }
+
+            public void Deconstruct(out int nextEntryId, out int nextSequence, out int nextBlock, out int nextIndexId)
+            {
+                nextEntryId = NextEntryId;
+                nextSequence = NextSequence;
+                nextBlock = NextBlock;
+                nextIndexId = NextIndexId;
+            }
+
+            public override string ToString() =>
+                $"Header[Next Entry Id: {NextEntryId}, Next Sequence: {NextSequence}, Next Block: {NextBlock}, Next Index Id: {NextIndexId}]";
+
+            public int NextEntryId { get; }
+            public int NextSequence { get; }
+            public int NextBlock { get; }
+            public int NextIndexId { get; }
+        }
+
         public Cache(string path)
         {
-            // TODO check if exists
             var mainDataPath = Path.Combine(path, DataFileName);
-            var masterIndexPath = Path.Combine(path, $"{IndexFilePrefixName}{MasterIndexId}");
+            if (!File.Exists(mainDataPath))
+            {
+                throw new FileNotFoundException($"{mainDataPath} cannot be found.");
+            }
+
             _mainDataFileStream = OpenFile(mainDataPath);
-            _masterIndexFileStream = OpenFile(masterIndexPath);
-            MasterIndex = new Index(MasterIndexId, _masterIndexFileStream);
-            var indexCount = MasterIndex.GetFileCount();
-            Indices = new Index[indexCount];
-            for (var id = 0; id < indexCount; id++)
+            _indices = new Dictionary<int, Index>(MaxIndexCount);
+            for (var id = 0; id < MaxIndexCount; id++)
             {
                 var indexPath = Path.Combine(path, $"{IndexFilePrefixName}{id}");
-                if (!File.Exists(indexPath))
+                if (File.Exists(indexPath) && !Directory.Exists(indexPath))
                 {
-                    throw new FileNotFoundException($"Problem loading index files. {indexPath} cannot be found.");
+                    var indexStream = OpenFile(indexPath);
+                    var index = new Index(id, indexStream);
+                    _indices[id] = index;
                 }
-                var indexStream = OpenFile(indexPath);
-                var index = new Index(id, indexStream);
-                Indices[id] = index;
             }
+
             // TODO provide custom fileaccess and filemode
             static FileStream OpenFile(string path) => File.Open(path, FileMode.Open, FileAccess.ReadWrite);
+        }
+
+        public byte[] Read(int type, int file)
+        {
+            var index = GetIndex(type);
+            var indexEntry = index.GetEntry(file);
+            var remainingBytes = indexEntry.Size;
+            var block = indexEntry.Offset;
+            var currentSequence = 0;
+
+            var large = file > ushort.MaxValue;
+            int blockHeaderSize = large ? BlockHeaderExtendedSize : BlockHeaderSize;
+            int blockChunkSize = large ? BlockChunkExtendedSize : BlockChunkSize;
+
+            var data = new List<byte>(indexEntry.Size);
+
+            while (remainingBytes > 0)
+            {
+                var blockData = new byte[TotalBlockSize];
+                _mainDataFileStream.Seek(block * TotalBlockSize, SeekOrigin.Begin);
+                _mainDataFileStream.Read(blockData, 0, blockData.Length);
+                var (nextEntryId, nextSequence, nextBlock, nextIndexId) =
+                    new Header(blockData[..blockHeaderSize], large);
+                var chunksConsumed = Math.Min(remainingBytes, blockChunkSize);
+                var totalConsumed = chunksConsumed + blockHeaderSize;
+
+                if (remainingBytes > 0)
+                {
+                    // TODO error checking
+                    data.AddRange(blockData[blockHeaderSize..totalConsumed]);
+                    remainingBytes -= chunksConsumed;
+                    block = nextBlock;
+                    currentSequence += 1;
+                }
+            }
+
+            return data.ToArray();
+        }
+
+
+        public Index GetIndex(int type) => !_indices.ContainsKey(type)
+            ? throw new KeyNotFoundException($"Given index {type} was not found.")
+            : _indices[type];
+
+        public void Dispose()
+        {
+            _mainDataFileStream.Dispose();
+            foreach (var (_, index) in _indices)
+            {
+                index.Dispose();
+            }
         }
     }
 }
--- JagCacheLib/Index.cs
+++ JagCacheLib/Index.cs
@@ -1,18 +1,53 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.IO;
 
 namespace JagCacheLib
 {
-    public class Index
+    public readonly struct IndexEntry
+    {
+        public IndexEntry(int id, int size, int offset)
+        {
+            Id = id;
+            Size = size;
+            Offset = offset;
+        }
+
+        public int Id { get; }
+        public int Size { get; }
+        public int Offset { get; }
+        
+        public override string ToString() =>
+            $"Index Entry[id: {Id}, size: {Size}, offset: {Offset}]";
+    }
+
+    public class Index : IDisposable
     {
         public const byte Size = 6;
-        
+
         public double Id { get; }
-        
+
         public FileStream IndexFileStream { get; }
-        
+
         public Index(int id, FileStream indexFileStream) => (Id, IndexFileStream) = (id, indexFileStream);
-        
+
+        public IndexEntry GetEntry(int entryId)
+        {
+            var pointer = entryId * Size;
+            // TODO check if the pointer is >= the index's size
+            var buffer = new byte[Size];
+            IndexFileStream.Seek(pointer, SeekOrigin.Begin);
+            IndexFileStream.Read(buffer, 0, buffer.Length);
+            int size = (buffer[0] << 16) | (buffer[1] << 8) | buffer[2];
+            int offset = (buffer[3] << 16) | (buffer[4] << 8) | buffer[5];
+            return new IndexEntry(entryId, size, offset);
+        }
+
         public int GetFileCount() => (int) (IndexFileStream.Length / Size);
+
+        public void Dispose()
+        {
+            IndexFileStream.Dispose();
+        }
     }
 }
